17d2f202676c36681b0463fd27b78a7b0b561734
jalvgrana <63903109+jalvgrana@users.noreply.github.com>
2021-12-14 16:53:19 +0100

    Delete Final Portfolio Submission W01-W14 directory

--- Final Portfolio Submission W01-W14/W11_13-Small Projects/06-Intermediate Algorithm Scripting/03-seek-and-destroy.js
+++
@@ -1,5 +0,0 @@
-function destroyer(arr, ...rest) {
-  return arr.filter(item =>rest.indexOf(item) === -1);
-}
-
-destroyer([1, 2, 3, 1, 2, 3], 2, 3);
--- Final Portfolio Submission W01-W14/W11_13-Small Projects/06-Intermediate Algorithm Scripting/05-spinal-tap-case.js
+++
@@ -1,5 +0,0 @@
-function spinalCase(str) {
-  return str.replace(/([A-Z])/g, ' $1').trim().split(/[\W_]/).filter(item => item !=='').map(item => item.toLowerCase()).join('-');
-}
-
-spinalCase('This Is Spinal Tap');
